908f7d0ef98aa17a024a406fbfb03027ae6a6fa4
Brandon Choi <bchoi28@gmail.com>
2023-11-02 03:28:21 -0400

    added dfs

--- src/components/FindMostPopularMovie/FindMostPopularMovie.jsx
+++ src/components/FindMostPopularMovie/FindMostPopularMovie.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { createPerson, addMovie, addFriend, findMostPopularMovieBFS, findMostPopularMovieDFS } from "../utils";
 import PersonList from "../PersonList";
 import MovieList from "../MovieList";
@@ -6,47 +6,35 @@ import './FindMostPopularMovie.css';
 
 const FindMostPopularMovie = () => {
 
-    // state variables for adding movies
-    const [selectedPerson, setSelectedPerson] = useState(null);
-    const [newMovie, setNewMovie] = useState("");
-
     // create people, Brandon being the root
-    const brandon = createPerson("Brandon");
-    const mark = createPerson("Mark");
-    const joe = createPerson("Joe");
-    const emily = createPerson("Emily");
-    const sarah = createPerson("Sarah");
-    const andrew = createPerson("Andrew");
-    const taylor = createPerson("Taylor");
+    const Brandon = createPerson("Brandon");
+    const Mark = createPerson("Mark");
+    const Joe = createPerson("Joe");
+    const Emily = createPerson("Emily");
+    const Sarah = createPerson("Sarah");
+    const Andrew = createPerson("Andrew");
+    const Taylor = createPerson("Taylor");
 
-    // create a person list array
-    const personList = [brandon, mark, joe, emily, sarah, andrew, taylor];
+    // create a person object list array
+    const personList = [Brandon, Mark, Joe, Emily, Sarah, Andrew, Taylor];
 
     // establish the network
-    addFriend(brandon, mark, joe);
-    addFriend(mark, emily, sarah);
-    addFriend(joe, andrew, taylor)
+    addFriend(Brandon, Mark, Joe);
+    addFriend(Mark, Emily, Sarah);
+    addFriend(Joe, Andrew, Taylor)
 
     // add liked movies to the network
-    addMovie(brandon, "Movie1", "Movie2", "Movie3");
-    addMovie(mark, "Movie1");
-    addMovie(joe, "Movie1", "Movie2", "Movie4");
-    addMovie(emily, "Movie1", "Movie2");
-    addMovie(sarah, "Movie3", "Movie4");
-    addMovie(andrew, "Movie2", "Movie3");
-    addMovie(taylor, "Movie1", "Movie4", "Movie5");
+    addMovie(Brandon, "Movie1", "Movie2", "Movie3");
+    addMovie(Mark, "Movie1");
+    addMovie(Joe, "Movie1", "Movie2", "Movie4");
+    addMovie(Emily, "Movie1", "Movie2");
+    addMovie(Sarah, "Movie3", "Movie4");
+    addMovie(Andrew, "Movie2", "Movie3");
+    addMovie(Taylor, "Movie1", "Movie4", "Movie5");
 
-    // find most popular movie of brandon's network
-    const { maxCount, mostPopularMovie } = findMostPopularMovieBFS(brandon);
-    // const { maxCount, mostPopularMovie } = findMostPopularMovieDFS(brandon);
-
-    // function to handle movie addition
-    const handleAddMovie = () => {
-        if (selectedPerson && newMovie) {
-            addMovie(selectedPerson, newMovie);
-            setNewMovie("");
-        }
-    };
+    // find most popular movie of Brandon's network
+    const { maxCount, mostPopularMovie } = findMostPopularMovieBFS(Brandon);
+    // const { maxCount, mostPopularMovie } = findMostPopularMovieDFS(Brandon);
 
     return (
         <div className="main-container">
@@ -55,20 +43,20 @@ const FindMostPopularMovie = () => {
                 <div className="network-container">
                     <div className="network-header">Network</div>
                     <div className="network-list">
-                        <PersonList person={brandon} />
+                        <PersonList person={Brandon} />
                     </div>
                 </div>
                 <div className="movies-container">
                     <div className="movies-header">Movies</div>
                     <div className="movies-list">
-                        <MovieList person={brandon} />
+                        <MovieList person={Brandon} />
                     </div>
                 </div>
             </div>
             <div className="result-container">The Most Popular Movie is:
                 <div className="result">{mostPopularMovie}</div>
                 <div>with a count of {maxCount}</div>
-                <div className="add-movie-bar" >
+                {/* <div className="add-movie-bar" >
                     <input
                         type="text"
                         placeholder="Enter movie name"
@@ -76,7 +64,7 @@ const FindMostPopularMovie = () => {
                         onChange={(e) => setNewMovie(e.target.value)}
                     />
                     <select
-                        value={selectedPerson ? selectedPerson.name : ""}
+                        value={selectedPerson}
                         onChange={(e) => setSelectedPerson(e.target.value)}
                     >
                         <option value="">Select a person</option>
@@ -87,7 +75,7 @@ const FindMostPopularMovie = () => {
                         ))}
                     </select>
                     <button onClick={handleAddMovie}>Add Movie</button>
-                </div>
+                </div> */}
             </div>
 
         </div>
