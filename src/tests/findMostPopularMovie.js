4a797f5218890420d85e12f5db1d6358d95ebd56
Brandon Choi <bchoi28@gmail.com>
2023-11-02 04:36:53 -0400

    added DFS to testing js file

--- src/tests/findMostPopularMovie.js
+++ src/tests/findMostPopularMovie.js
@@ -32,4 +32,35 @@ function findMostPopularMovieBFS(root) {
     else return mostPopularMovies[0];
 };
 
+// function findMostPopularMovieDFS(root) {
+
+//     const movieCount = new Map();
+//     let maxCount = 0;
+//     let mostPopularMovie;
+
+//     const dfs = (person) => {
+//         for (const movie of person.movies) {
+//             if (!movieCount.has(movie)) {
+//                 movieCount.set(movie, 0);
+//             }
+
+//             const count = movieCount.get(movie) + 1;
+//             movieCount.set(movie, count);
+
+//             if (count > maxCount) {
+//                 maxCount = count;
+//                 mostPopularMovie = movie;
+//             }
+//         }
+
+//         for (const friend of person.friends) {
+//             dfs(friend);
+//         }
+//     };
+
+//     dfs(root);
+
+//     return { maxCount, mostPopularMovie };
+// };
+
 module.exports = findMostPopularMovieBFS;
